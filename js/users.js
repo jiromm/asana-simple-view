833aae1dbfb41de872252595b288ef71ed6a60b0
Aram Baghdasaryan <xquack@gmail.com>
2015-09-28 22:17:38 +0400

    Users has been added to project

---
+++ js/users.js
@@ -0,0 +1,58 @@
+var users = [
+	{
+		"id": 10124585031830,
+		"name": "Aram Baghdasaryan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/10124585031830.JrvDk4UBi3WC0VX8vMT7_60x60.png"
+		}
+	},
+	{
+		"id": 12950252745597,
+		"name": "Arbi Baghoomian",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/12950252745597.xddYtxsUKBXFP4oYLpSF_60x60.png"
+		}
+	},
+	{
+		"id": 10124585031848,
+		"name": "Eduard Aleksanyan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/10124585031848.wyUMUWeGEyfd7bXATcGa_60x60.png"
+		}
+	},
+	{
+		"id": 45707791044342,
+		"name": "Harut Grigoryan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/45707791044342.6ghgdwzni7VLBsuOvMXL_60x60.png"
+		}
+	},
+	{
+		"id": 25719136431202,
+		"name": "Hrayr Papikyan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/25719136431202.G39up5gCwyUO8Lg5UhBL_60x60.png"
+		}
+	},
+	{
+		"id": 5754650264628,
+		"name": "Tigran Tadevosyan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/5754650264628.J6jyaDiKxliFxx4kIpiU_60x60.png"
+		}
+	},
+	{
+		"id": 10124585031839,
+		"name": "Tigran Petrosyan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/10124585031839.Zvk5gKoGkI4BZNXFeAG7_60x60.png"
+		}
+	},
+	{
+		"id": 11686527531417,
+		"name": "Tigran Ghabuzyan",
+		"photo": {
+			"image_60x60": "https://s3.amazonaws.com/profile_photos/11686527531417.QOvoSX7R6lTJSG9VDBQI_60x60.png"
+		}
+	}
+];
